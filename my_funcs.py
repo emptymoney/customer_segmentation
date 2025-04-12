6396ce4dbf6fca9542e6e1cfd23f9124d62905b1
emptymoney <44281153+emptymoney@users.noreply.github.com>
2025-04-12 17:10:54 +0700

    Add files via upload

--- my_funcs.py
+++ my_funcs.py
@@ -1,7 +1,6 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import squarify
 import plotly.express as px
 
 # -----------------------------------------------------------------------------------
